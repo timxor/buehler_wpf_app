20b1c27fa01fba0cece09c4ee2a903a41da335ba
Tim Siwula <siwulactim@gmail.com>
2023-12-19 20:48:24 -0600

    done

---
+++ WPF-LoginForm/View/RegisterView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+
+namespace WPF_LoginForm.View
+{
+    /// <summary>
+    /// Interaction logic for RegisterView.xaml
+    /// </summary>
+    public partial class RegisterView : Window
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
